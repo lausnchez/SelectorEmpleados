bc9738be4faa71eebe84c8e8d594127e9c858d43
lausnchez <lausnchezmartn@gmail.com>
2025-01-14 18:16:47 +0100

    Vista hecha

---
+++ empleadosNoSeQue/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace empleadosNoSeQue
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_leerTodos_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btn_limpiar_Click(object sender, RoutedEventArgs e)
+        {
+            //OpenFileDialog
+        }
+    }
+}
