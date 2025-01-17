2c5157139d66bb5f5178863e2825555c15ef2b68
lausnchez <lausnchezmartn@gmail.com>
2025-01-17 20:31:44 +0100

    Agregar cifrado

---
+++ empleadosNoSeQue/Cifrado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace empleadosNoSeQue
+{
+    internal class Cifrado
+    {
+        public String cifrar(String cadena)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(cadena));
+        }
+
+        public String descrifrar(String cadena)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(cadena));
+        }
+    }
+}
--- empleadosNoSeQue/Login.xaml.cs
+++ empleadosNoSeQue/Login.xaml.cs
@@ -23,5 +23,15 @@ namespace empleadosNoSeQue
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
