826f79a02ebbfd9a6ae8c0bda99b11c730335a3a
lausnchez <lausnchezmartn@gmail.com>
2025-01-24 14:07:43 +0100

    empezar listado empleados

--- empleadosNoSeQue/MainWindow.xaml.cs
+++ empleadosNoSeQue/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,20 +23,84 @@ namespace empleadosNoSeQue
     /// </summary>
     public partial class MainWindow : Window
     {
+        String nombreFichero = "empleados.txt";
+        StreamWriter sw = null;
+        StreamReader sr = null;
+        List<Empleado> listaEmpleados = new List<Empleado>();
+
         public MainWindow(String usuario)
         {
             InitializeComponent();
             lbl_nombreUsuario.Content = usuario;
         }
 
+        public class Empleado
+        {
+            public int ID {  get; set; }
+            public String Nombre {  get; set; }
+            public String Apellidos {  get; set; }
+            public String Telefono {  get; set; }
+
+            public Empleado(int id, String nombre, String apellidos, String tlf)
+            {
+                this.ID = id;
+                this.Nombre = nombre;
+                this.Apellidos = apellidos;
+                this.Telefono = tlf;
+            }
+        }
+
         private void btn_leerTodos_Click(object sender, RoutedEventArgs e)
         {
-            
+            sr = new StreamReader(nombreFichero);
+            String linea = "";
+            int counter = 0;
+            int id = 0;
+            String nombre = "";
+            String apellidos = "";
+            String tlf = "";
+
+            while ((linea = sr.ReadLine()) != null)
+            {
+                // ID
+                if (counter%4 == 0)
+                {
+                    id = int.Parse(linea);
+                }
+                // NOMBRE
+                else if (counter%4 == 1)
+                {
+                    nombre = linea;
+                }
+                // APELLIDOS
+                else if (counter%4 == 2)
+                {
+                    apellidos = linea;
+                }
+                // TELEFONO
+                else if (counter%4 == 3)
+                {
+                    tlf = linea;
+                    listaEmpleados.Add(new Empleado(id, nombre, apellidos, tlf));
+                    // Reset Strings
+                    id = 0;
+                    nombre = "";
+                    apellidos = "";
+                    tlf = "";
+                }
+                counter++;
+            }
+            sr.Close();
         }
 
         private void btn_limpiar_Click(object sender, RoutedEventArgs e)
         {
             //OpenFileDialog
+        }
+
+        private void btn_guardar_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
